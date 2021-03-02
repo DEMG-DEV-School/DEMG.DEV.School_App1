45dcc7f91b9e14be284dfa82bb14c945e5747764
David Mendez <dmendez@ihconsulting.mx>
2021-03-02 12:41:51 -0600

    Add project files.

---
+++ DEMG.DEV.School_App1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DEMG.DEV.School_App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
